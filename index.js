2ba60f6e0071ab1b80c2eceb3f4fe6f547de19bf
purgatorygates <ulisesrivero1984@gmail.com>
2020-10-11 23:58:20 -0400

    final working code

--- index.js
+++ index.js
@@ -1,55 +1,82 @@
 const prompt = require("readline-sync");
 
-const input = prompt.question("yes to play, no");
+console.log("Welcome to the World Campions Tournament");
 
-// console.log(input)
-//break out of loop after 2 wins or 3 rounds
+let playerSelect = prompt.question(`please enter your name: \n`);
+
+const input = prompt.question(
+  `${playerSelect} Are you ready to battle your first opponenet?: Type Yes:\n`
+);
+
+// function restart() {
+//   const letsPlay = prompt.keyInSelect(
+//     ["Yes", "No"],
+//     "do you want to play again?"
+//   );
+//   if (letsPlay === 0) {
+//     console.log("A brave choice..");
+//   } else {
+//     console.log("Oh well.. Maybe next time.");
+//     process.exit();
+//   }
+// }
 
 if (input.toLowerCase() === "yes") {
   let userWins = 0;
   let maxRounds = 0;
   let losses = 0;
   while (userWins <= 1 && maxRounds <= 2) {
-    let enemy = ["0", "1", "2"];
-    let playerInput = prompt.question("choose 0, 1, 2");
-    console.log(`This is the player input = ${playerInput}`);
+    let enemy = ["punch", "kick", "throw"];
+    let playerInput = prompt.question("choose punch, kick, or throw: ");
+    console.log(``);
     let opponent = enemy[Math.floor(Math.random() * 3)];
     let you = playerInput;
     if (opponent === you) {
+      console.log(`You used ${playerInput} and your opponent used ${opponent}`);
       console.log("draw");
       console.log(`Rounds = ${maxRounds}`);
-    } else if (you === "0" && opponent === "1") {
-      console.log(`you win, ${opponent} !`);
-      userWins++;
       console.log(`Total user wins = ${userWins}`);
       console.log(`Total user losses = ${losses}`);
+    } else if (you === "punch" && opponent === "kick") {
+      console.log(
+        `you win! You used ${playerInput} and your opponent used ${opponent}`
+      );
+      userWins++;
       maxRounds++;
       console.log(`Rounds = ${maxRounds}`);
-    } else if (you === "1" && opponent === "2") {
-      console.log(`you win, ${opponent} ?`);
-      userWins++;
       console.log(`Total user wins = ${userWins}`);
       console.log(`Total user losses = ${losses}`);
+    } else if (you === "kick" && opponent === "throw") {
+      console.log(
+        `You win!,You used ${playerInput} and your opponent used ${opponent}`
+      );
+      userWins++;
       maxRounds++;
       console.log(`Rounds = ${maxRounds}`);
-    } else if (you === "2" && opponent === "0") {
-      console.log(`you win, ${opponent} ++`);
+      console.log(`Total user wins = ${userWins}`);
+      console.log(`Total user losses = ${losses}`);
+    } else if (you === "throw" && opponent === "punch") {
+      console.log(
+        `You win!,You used ${playerInput} and your opponent used ${opponent}`
+      );
+      maxRounds++;
       userWins++;
       console.log(`Total user wins = ${userWins}`);
       console.log(`Total user losses = ${losses}`);
-      maxRounds++;
       console.log(`Rounds = ${maxRounds}`);
     } else {
-      console.log(`you lose, the computer had the number: ${opponent}`);
-      maxRounds++;
-      losses++;
+      console.log(
+        `You Lose!,You used ${playerInput} and your opponent used ${opponent}`
+      );
       console.log(`Total user wins = ${userWins}`);
       console.log(`Total user losses = ${losses}`);
       console.log(`Rounds = ${maxRounds}`);
+      maxRounds++;
+      losses++;
     }
   }
 }
 
-// function restart(){
-//   console.log('do you want to play again')
-// }
+console.log(
+  `Thanks you for playing, stay tuned for future updates and remember to Like, Sub, and Subscribe\n Goodbye ${playerSelect}`
+);
